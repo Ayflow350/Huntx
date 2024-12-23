5e9c95db49c90a5c96f27076153ce6d7d7618151
Ayflow350 <soluwatist@gmail.com>
2024-12-23 23:03:00 +0100

    the mobile text adjusted

--- app/(auth)/SignUp/page.tsx
+++ app/(auth)/SignUp/page.tsx
@@ -66,7 +66,7 @@ const SignUp = () => {
               Get more opportunities{" "}
             </h1>
           </div>
-          <button className="border-[#CCCCF5] mx-auto flex flex-row gap-x-2  border-2 px-[100px] py-[8px] lg:py-[12px] lg:px-[108px]  font-epilogue text-[#4640DE] justify-center items-center font-bold text-[16px] ">
+          <button className="border-[#CCCCF5] mx-auto flex flex-row gap-x-2  border-2 px-[70px] py-[8px] lg:py-[12px] lg:px-[108px]  font-epilogue text-[#4640DE] justify-center items-center font-bold text-[12px] lg:text-[16px] ">
             <span>
               <Image
                 src={GoggleLogo}
@@ -78,22 +78,24 @@ const SignUp = () => {
           </button>
           <div className="flex items-center text-[#202430]  justify-center my-8">
             <div className="border-t border-gray-300 text-[#202430] flex-grow mr-3"></div>
-            <span className="text-gray-500 text-base">
+            <span className="text-gray-500 text-[12px] md:text-base">
               Or sign up with email
             </span>
             <div className="border-t border-gray-300 text-[#202430] flex-grow ml-3"></div>
           </div>
           <div className=" space-y-[22px] px-4">
             <div className="text-left">
-              <h1 className="text-[#515B6F] font-semibold mb-2">Full Name</h1>
+              <h1 className="text-[#515B6F] font-semibold mb-2 text-[12px] md:text-base">
+                Full Name
+              </h1>
               <input
                 type="text"
                 placeholder="Enter your full name"
-                className="border-[1.5px] py-[12px] px-[16px] w-full outline-none"
+                className="border-[1.5px] py-[12px] px-[16px] w-full text-[12px] md:text-base outline-none"
               />
             </div>
-            <div className="text-left">
-              <h1 className="text-[#515B6F] font-semibold mb-2">
+            <div className="text-left text-[12px] md:text-base">
+              <h1 className="text-[#515B6F] font-semibold mb-2 ">
                 Email Address
               </h1>
               <input
@@ -102,7 +104,7 @@ const SignUp = () => {
                 className="border-[1.5px] py-[12px] px-[16px] w-full outline-none"
               />
             </div>
-            <div className="text-left">
+            <div className="text-left text-[12px] md:text-base">
               <h1 className="text-[#515B6F] font-semibold mb-2">Password</h1>
               <input
                 type="text"
@@ -113,13 +115,13 @@ const SignUp = () => {
             <button className="bg-[#4640DE] font-epilogue font-bold text-white py-[12px] w-full">
               Continue
             </button>
-            <h1 className="text-[#202430] text-base">
+            <h1 className="text-[#202430] text-[12px] md:text-base">
               Already have an account?{" "}
               <span className="text-[#4640DE] font-semibold">Login</span>
             </h1>
           </div>
           <div className="">
-            <p className="w-[400px] text-[#7C8493] text-left text-[14px]">
+            <p className="w-[400px] text-[#7C8493] text-left md:text-[14px] text-[12px] ">
               By clicking 'Continue', you acknowledge that you have read and
               accept the <span className="text-blue-500">Terms of Service</span>{" "}
               and <span className="text-blue-500">Privacy Policy</span>.
